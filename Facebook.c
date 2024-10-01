affcabd3e10e0088d4fc2fd46d2484ae1fa73a33
Chshanmukha2005 <146986782+Chshanmukha2005@users.noreply.github.com>
2024-10-01 20:44:21 +0530

    Add files via upload

---
+++ Facebook.c
@@ -0,0 +1,16 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    n=int(input())
+    l=list(map(int,input().split()))
+    c=list(map(int,input().split()))
+    maxl=max(l)
+    maxc=0
+    if(l.count(maxl)>1):
+        for j in range(len(l)):
+            if l[j]==maxl:
+                if(c[j]>maxc):
+                    maxc=c[j]
+        print(c.index(maxc)+1)
+    else:
+        print(l.index(maxl)+1)
